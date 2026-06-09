0e5547788a93f86c22ec8b60b269cba04ece249a
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-06-08 21:43:17 -0500

    got some good progress. schema and seed are done. create query files.

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,28 @@
 import db from "#db/client";
+import { createPlaylist } from "#db/queries/playlists";
+import { createTrack } from "#db/queries/tracks";
+import { addTrackToPlaylist } from "#db/queries/playlist_tracks";
 
 await db.connect();
 await seed();
 await db.end();
+
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  const playlists = [];
+  const tracks = [];
+  for (let i = 1; i <= 10; i++) {
+    playlists.push(await createPlaylist("Playlist " + i));
+  }
+  for (let j = 1; j <= 20; j++) {
+    tracks.push(await createTrack("Track " + j, 1000 * j));
+  }
+  let count = 0;
+  for (let i = 0; i < playlists.length && count < 15; i++) {
+    for (let j = 0; j < tracks.length && count < 15; j++) {
+      await addTrackToPlaylist(playlists[i].id, tracks[j].id);
+      count++;
+    }
+  }
 }
