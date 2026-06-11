bd5d1b85f0ef0430be4f7651296229ed555155c6
Tyler Sherrick <tyler.sherrick@gmail.com>
2026-06-11 18:31:11 -0500

    finished the rest of the assignment

--- db/seed.js
+++ db/seed.js
@@ -1,28 +1,20 @@
 import db from "#db/client";
 import { createPlaylist } from "#db/queries/playlists";
+import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { createTrack } from "#db/queries/tracks";
-import { addTrackToPlaylist } from "#db/queries/playlist_tracks";
 
 await db.connect();
 await seed();
 await db.end();
-
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  const playlists = [];
-  const tracks = [];
-  for (let i = 1; i <= 10; i++) {
-    playlists.push(await createPlaylist("Playlist " + i));
+  for (let i = 1; i <= 20; i++) {
+    await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
+    await createTrack("Track " + i, i * 50000);
   }
-  for (let j = 1; j <= 20; j++) {
-    tracks.push(await createTrack("Track " + j, 1000 * j));
-  }
-  let count = 0;
-  for (let i = 0; i < playlists.length && count < 15; i++) {
-    for (let j = 0; j < tracks.length && count < 15; j++) {
-      await addTrackToPlaylist(playlists[i].id, tracks[j].id);
-      count++;
-    }
+  for (let i = 1; i <= 15; i++) {
+    const playlistId = 1 + Math.floor(i / 2);
+    await createPlaylistTrack(playlistId, i);
   }
 }
